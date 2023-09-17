fix: DCM formula according to Parisi's correction

TP3/scripts/DCM.py
@@ -69,9 +69,9 @@ def calculate_diffusion_coefficient(DCM_values, frames):
     print(f"length of frames: {len(frames[0:len(tiempo)])} - frames = {str(frames[0:len(tiempo)])}")
 
     # Realizar una regresión lineal para obtener la pendiente y el intercepto
-    pendiente, _ = np.polyfit(frames[0:len(tiempo)], real_values, 1)
+    pendiente, b = np.polyfit(frames[0:len(tiempo)], real_values, 1)
 
-    return pendiente / 2, value
+    return pendiente / 4, value, b
 
 def plot_DCM(dictionary, N_particles, Lsize):
     frames = list(dictionary.keys())
@@ -92,10 +92,10 @@ def plot_DCM(dictionary, N_particles, Lsize):
     ax.set_xticks(x)
     ax.set_xticklabels(x_labels, rotation=45, ha="right")
 
-    D, break_line = calculate_diffusion_coefficient(DCM_values[1:len(DCM_values)], frames[1:len(frames)])
+    D, break_line, b = calculate_diffusion_coefficient(DCM_values[1:len(DCM_values)], frames[1:len(frames)])
 
     # Agregar la línea de regresión solo hasta el punto de break_line
-    y_regresion = np.array(frames[1:break_line]) * 2 * D + np.mean(DCM_values[1:4]) # y = mx + b
+    y_regresion = np.array(frames[1:break_line]) * 4 * D + b # y = mx + b
     print("breakline: " + str(break_line))
     print("frames: " + str(frames[1:break_line]))
     print("m: " + str(2 * D))
@@ -109,12 +109,12 @@ def plot_DCM(dictionary, N_particles, Lsize):
 
 
 if UNIQUE:
-    displacements = process_simulation_output(f"./data/output/Dynamic_N_{N[0]}_L_{L[0]}.dump", considered_particles=N[0], interval=INTERVAL_LENGTH)
+    displacements = process_simulation_output(f"./data/output/Dynamic_N_{N[0]}_L_{L[0]}_v_1.dump", considered_particles=N[0], interval=INTERVAL_LENGTH)
     plot_DCM(displacements, N[0], L[0])
     print(f'N = {N[0]}, L = {L[0]}: Done')
 else:
     for i in range(len(N)):
         for j in range(len(L)):
-            displacements = process_simulation_output(f"./data/output/Dynamic_N_{N[i]}_L_{L[j]}.dump", considered_particles=N[i], interval=INTERVAL_LENGTH)
+            displacements = process_simulation_output(f"./data/output/Dynamic_N_{N[i]}_L_{L[j]}_v_1.dump", considered_particles=N[i], interval=INTERVAL_LENGTH)
             plot_DCM(displacements, N[i], L[j])
             print(f'N = {N[i]}, L = {L[j]}: Done')